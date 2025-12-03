a589475e344b2b35363d34de99b885c729d7f5f4
manthan3678 <manthangedam198@gmail.com>
2025-12-03 20:52:46 +0530

    Pattern Practice

---
+++ star-pattern.js
@@ -0,0 +1,133 @@
+
+let n=3;
+let num =1;
+for (let i=1; i<=n; i++ ){
+    let row='';
+    for(let j=1;j<=n; j++){
+        row+=num + " "
+        num++
+    }
+    console.log(row)
+}
+
+// 1 2 3 
+// 4 5 6 
+// 7 8 9 
+// *******************************************************************
+
+let chr = 65;
+for (let i=1; i<=n; i++){
+
+    let row='';
+    
+    for(let j=1; j<=n; j++){
+    row += String.fromCharCode(chr) + " "
+    chr++
+    }
+    console.log(row);
+}
+
+// A B C 
+// D E F 
+// G H I
+// *********************************************************************
+for (let i=1; i<=n; i++){
+    let row='';
+    for (let j=1; j<=i;j++){
+        row += i;
+    }
+    console.log(row);
+}
+
+// 1
+// 22
+// 333
+// *********************************************************************
+//  let chr=65;
+for (let i=1; i<=n; i++){
+    let row='';
+   
+    for (let j=1; j<=i;j++){
+        row += String.fromCharCode(chr);
+    
+    }
+    chr++
+    console.log(row);
+}
+// A
+// BB
+// CCC
+// **********************************************************************
+
+for (let i=1; i<=n; i++){
+    let row='';
+    for (let j=i; j>=1;j--){
+        row += j;
+    }
+    console.log(row);
+}
+// 1
+// 21
+// 321
+// ********************************************************************
+
+let nu=1
+for (let i=1; i<=n; i++){
+    let row='';
+    for (let j=1; j<=i;j++){
+        row += nu;
+        nu++
+    }
+    console.log(row);
+}
+// 1
+// 23
+// 456
+// ********************************************************************
+// let chr=65;
+for (let i=1; i<=n; i++){
+    let row='';
+   
+    for (let j=1; j<=i;j++){
+        row += String.fromCharCode(chr);
+        chr++
+    }
+    console.log(row);
+}
+// A
+// BC
+// DEF
+// ******************************************************************
+// let n=4;
+for(let i=1;i<=n;i++ ){
+    let row='';
+    for(let j=1;j<=i;j++){
+        row+=" "
+    }
+    for(let j=n;j>=i;j--){
+        row+=i
+    }
+    console.log(row)
+}
+//  1111
+//   222
+//    33
+//     4
+// *******************************************************************
+// let n=4;
+// let chr=65;
+for(let i=1;i<=n;i++ ){
+    let row='';
+    for(let j=1;j<=i;j++){
+        row+=" "
+    }
+    for(let j=n;j>=i;j--){
+        row+=String.fromCharCode(chr);
+    }
+    chr++
+    console.log(row)
+}
+//  AAAA
+//   BBB
+//    CC
+//     D
